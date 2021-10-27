2b99e115aa5bbe7203b1dd9e63e435b951aa2568
Jean Iwanowski <jean.iwanowski@edelweissloisirs.ch>
2021-10-27 21:02:27 +0200

    add category validation

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 function updateValidation() {
   let validationHandler = new ValidationHandler
-  validationHandler.add(new Validation('Sujets', 'B2:B', 'Réunions', 'A2:A', ['à planifier']))
+  validationHandler.add(new Validation('Sujets', 'B2:B', 'Réunions', 'A2:A', false, ['à planifier']))
   validationHandler.add(new Validation('Sujets', 'C2:C', 'Personnes', 'A2:A'))
   validationHandler.add(new Validation('Réunions', 'D2:D', 'Personnes', 'A2:A'))
+  validationHandler.add(new Validation('Sujets', 'D2:D', 'Sujets', 'D2:D', true))
   validationHandler.update()
 }
 
